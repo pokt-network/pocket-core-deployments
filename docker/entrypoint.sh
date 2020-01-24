859ab7503a16d91284caf0d9eae8a7a5210fb551
aqt01 <lowell.abbott@gmail.com>
2020-01-24 13:45:34 -0400

    Add POCKET_CORE_GENESIS and POCKET_CORE_CHAINS to be passed as env variable for docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,4 +2,12 @@
 
 set -e
 
+if ${POCKET_CORE_GENESIS:-false} ; then
+    echo "${POCKET_CORE_GENESIS}" > /home/app/.pocket/genesis.json
+fi
+
+if ${POCKET_CORE_CHAINS:-false} ; then
+    echo "${POCKET_CORE_CHAINS}" > /home/app/.pocket/chains.json
+fi
+
 exec "$@"
