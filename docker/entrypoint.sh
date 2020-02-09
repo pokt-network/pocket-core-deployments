37461abe51043688cf18e8bfcbd8caed9bbe9cb6
aqt01 <lowell.abbott@gmail.com>
2020-02-09 17:12:17 -0400

    Undoing unnecessary if logic

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,15 +4,11 @@ set -e
 
 mkdir -p /home/app/.pocket/config
 
-if [ -z "${POCKET_CORE_GENESIS}" ] ; then
-    true
-else
+if [ -n "${POCKET_CORE_GENESIS}" ] ; then
     echo "${POCKET_CORE_GENESIS}" > /home/app/.pocket/config/genesis.json
 fi
 
-if [ -z "${POCKET_CORE_CHAINS}" ] ; then
-    true
-else
+if [ -n "${POCKET_CORE_CHAINS}" ] ; then
     echo "${POCKET_CORE_CHAINS}" > /home/app/.pocket/config/chains.json
 fi
 
