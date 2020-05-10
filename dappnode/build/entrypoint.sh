07ea90c9175aca82c5dc25e52587d7a6bdd98351
julio-jimenez <juliofrancisj@gmail.com>
2020-05-10 18:56:23 -0400

    bump pocket-core to RC-0.3.1

--- dappnode/build/entrypoint.sh
+++ dappnode/build/entrypoint.sh
@@ -12,4 +12,8 @@ if [ -n "${POCKET_CORE_CHAINS}" ] ; then
     echo "${POCKET_CORE_CHAINS}" > /home/app/.pocket/config/chains.json
 fi
 
+if [ -n "${POCKET_CORE_CONFIG}" ] ; then
+    echo "${POCKET_CORE_CONFIG}" > /home/app/.pocket/config/config.json
+fi
+
 /usr/bin/expect /home/app/command.sh $@
