185258ab6440611c4f570ca07694df80dfe13dd8
julio-jimenez <juliofrancisj@gmail.com>
2020-05-20 12:27:31 -0400

    push multiple tags to docker hub

--- docker/command.sh
+++ docker/command.sh
@@ -3,10 +3,16 @@
 set command $argv; # Grab the first command line parameter
 set timeout -1
 
+spawn sh -c "cp /tmp/*.json /home/app/.pocket/config/"
+sleep 2
 if { $env(POCKET_CORE_KEY) eq "" }  {
+    log_user 0
     spawn sh -c "$command"
+    send -- "$env(POCKET_CORE_PASSPHRASE)\n"
+    log_user 1
 
 } else {
+    log_user 0
     spawn pocket accounts import-raw $env(POCKET_CORE_KEY)
     sleep 1
     send -- "$env(POCKET_CORE_PASSPHRASE)\n"
@@ -15,12 +21,9 @@ if { $env(POCKET_CORE_KEY) eq "" }  {
     sleep 1
     send -- "$env(POCKET_CORE_PASSPHRASE)\n"
     expect eof
+    log_user 1
     spawn sh -c "$command"
 }
-
-sleep 1
-
-send -- "$env(POCKET_CORE_PASSPHRASE)\n"
 
 expect eof
 
