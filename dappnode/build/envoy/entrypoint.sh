145f8610239f4a77cf9177a84375365fcd3528be
julio-jimenez <juliofrancisj@gmail.com>
2020-06-18 13:25:54 -0400

    add envoy files

---
+++ dappnode/build/envoy/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+case ${TLS_OPTION} in
+"I have a certificate")
+    cp /tmp/certificate.crt /tmp/key.key /etc/envoy/
+    /home/app/entrypoint.sh & envoy -c /etc/envoy/envoy.yaml --component-log-level upstream:debug,connection:trace
+    ;;
+"Do not use a certificate")
+    /home/app/entrypoint.sh
+    ;;
+"Provide a selfsigned certificate for me")
+
+    if test -f "/etc/envoy/certificate.crt" && test -f "/etc/envoy/key.key"; then
+        /home/app/entrypoint.sh
+    else
+        sh certificate.sh
+        wait 5
+        /home/app/entrypoint.sh & envoy -c /etc/envoy/envoy.yaml --component-log-level upstream:debug,connection:trace
+    fi
+    ;;
+esac
